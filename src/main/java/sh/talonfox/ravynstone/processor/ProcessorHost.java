09a55af36fb33a7942ae0de1e3ff7889a37c841f
TalonFox <105325988+Talon396@users.noreply.github.com>
2022-07-31 07:40:17 -0500

    Update ProcessorHost.java

--- src/main/java/sh/talonfox/ravynstone/processor/ProcessorHost.java
+++ src/main/java/sh/talonfox/ravynstone/processor/ProcessorHost.java
@@ -1,19 +1,6 @@
 package sh.talonfox.ravynstone.processor;
 
 public interface ProcessorHost {
-    /*
-    var targetBus: Byte
-  val isBusConnected: Boolean
-  fun bus(): Device?
-
-  fun resetBusState()
-  var allowWrite: Boolean
-
-  var writePos: Short
-
-  fun memRead(at: Short): Byte
-  fun memStore(at: Short, data: Byte)
-     */
     byte targetBus = 0;
     Boolean isBusConnected = false;
 
