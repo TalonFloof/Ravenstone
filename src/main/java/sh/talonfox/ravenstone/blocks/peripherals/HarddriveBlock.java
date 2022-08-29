a91b5535a0b355c795348540374442790d2031d9
TalonFox <105325988+Talon396@users.noreply.github.com>
2022-08-29 18:34:01 -0500

    A thing I'm working on
    Signed-off-by: TalonFox <105325988+Talon396@users.noreply.github.com>

--- src/main/java/sh/talonfox/ravenstone/blocks/peripherals/HarddriveBlock.java
+++ src/main/java/sh/talonfox/ravenstone/blocks/peripherals/HarddriveBlock.java
@@ -1,4 +1,18 @@
 package sh.talonfox.ravenstone.blocks.peripherals;
 
-public class HarddriveBlock {
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.util.math.BlockPos;
+import org.jetbrains.annotations.Nullable;
+
+public class HarddriveBlock extends PeripheralBlock {
+    public HarddriveBlock(Settings settings) {
+        super(settings);
+    }
+
+    @Nullable
+    @Override
+    public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
+        return new HarddriveBlockEntity(pos, state);
+    }
 }
--- src/main/java/sh/talonfox/ravenstone/blocks/peripherals/HarddriveBlockEntity.java
+++ src/main/java/sh/talonfox/ravenstone/blocks/peripherals/HarddriveBlockEntity.java
@@ -1,4 +1,11 @@
 package sh.talonfox.ravenstone.blocks.peripherals;
 
-public class HarddriveBlockEntity {
+import net.minecraft.block.BlockState;
+import net.minecraft.util.math.BlockPos;
+import sh.talonfox.ravenstone.blocks.BlockRegister;
+
+public class HarddriveBlockEntity extends PeripheralBlockEntity {
+    public HarddriveBlockEntity(BlockPos pos, BlockState state) {
+        super(BlockRegister.RAVEN_HARDDRIVE_ENTITY, pos, state, 2);
+    }
 }
